88943f6e67cf2a7088023c2ee2ad0c892e5b13ea
nborwankar <nborwankar@gmail.com>
2012-04-08 00:54:01 -0700

    Finish lyout and routes. Signup link and working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,26 +6,26 @@ describe "StaticPages" do
   describe "Home page" do
     before { visit root_path }
     it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: "Ruby on Rails Tutorial Sample App") }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => " | Help") }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_selector('h1', :text => 'About') }
-    it { should have_selector('title', :text => " | About") }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text:  full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
     it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 
 end
